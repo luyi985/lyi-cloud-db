1ffe5c5e0188b16f31743a47e105bff05b23a405
LuYi <luyi985@gmail.com>
2018-09-16 18:27:43 +1000

    response body

--- handler.js
+++ handler.js
@@ -1,3 +1,9 @@
 module.exports.hello = (event, context, callback) => {
-    callback(null, "hello");
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify({
+          message: `Hello, the current time is ${new Date().toTimeString()}.`,
+        }),
+      };
+    callback(null, response);
 }
