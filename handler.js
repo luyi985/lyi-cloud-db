2dc820ab0a2cc0277a3fc2c2d342e7624d753c3c
LuYi <luyi985@gmail.com>
2018-09-17 22:10:52 +1000

    slack notification

---
+++ handler.js
@@ -0,0 +1,9 @@
+module.exports.hello = (event, context, callback) => {
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify({
+          message: `Hello, the current time is ${new Date().toTimeString()}.`,
+        }),
+      };
+    callback(null, response);
+}
