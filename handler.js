78bfab5b06e860b9aa28ae00682b878a51d5f7fa
luyi985 <LYi@bauer-media.com.au>
2018-09-27 14:47:21 +1000

    Feature/graphql with lambda (#8)
    * branch build update
    
    * explor commit message in template
    
    * install packages
    
    * setup lambda-graphql
    
    * fix http trigger
    
    * event method
    
    * correct graphql-tool dependency
    
    * fix schema
    
    * add graphql package
    
    * fix handler import
    
    * add test
    
    * fix import handler issue.1
    
    * fix import handler issue.2
    
    * fix import handler issue.3
    
    * recover excutable schema
    
    * fix makeExcutableSchema

--- handler.js
+++ handler.js
@@ -1,9 +1,32 @@
-module.exports.hello = (event, context, callback) => {
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify({
-          message: `Hello, the current time is ${new Date().toTimeString()}.`,
-        }),
-      };
-    callback(null, response);
+const { ApolloServer, makeExecutableSchema } = require('apollo-server-lambda');
+const typeDefs = require('./schema');
+const resolvers = require('./resolvers');
+
+const executableSchema = makeExecutableSchema({
+  typeDefs,
+  resolvers,
+});
+
+const server = new ApolloServer({ schema: executableSchema, logger: e => console.log(e) });
+
+const graphqlHandler = server.createHandler({
+  cors: {
+    origin: '*',
+    credentials: true,
+  },
+});
+
+const test = (event, context, callback) => {
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+    message: `Hello, the current time is ${new Date().toTimeString()}.`,
+    })
+  };
+  callback(null, response);
+}
+
+module.exports = {
+  graphqlHandler,
+  test
 }
