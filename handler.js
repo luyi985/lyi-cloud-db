add08b9b9c67716c6ba30814d061b0eeea9cc3b6
LuYi <luyi985@gmail.com>
2018-09-27 13:58:38 +1000

    recover excutable schema

--- handler.js
+++ handler.js
@@ -1,19 +1,14 @@
 const { ApolloServer } = require('apollo-server-lambda');
-//const  { makeExecutableSchema } = ApolloServer;
+const  { makeExecutableSchema } = ApolloServer;
 const typeDefs = require('./schema');
 const resolvers = require('./resolvers');
 
-// const executableSchema = makeExecutableSchema({
-//   typeDefs,
-//   resolvers,
-// });
-
-//const server = new ApolloServer({ schema: executableSchema, logger: e => console.log(e) });
-const server = new ApolloServer({ 
+const executableSchema = makeExecutableSchema({
   typeDefs,
   resolvers,
-  logger: e => console.log(e)
 });
+
+const server = new ApolloServer({ schema: executableSchema, logger: e => console.log(e) });
 
 const graphqlHandler = server.createHandler({
   cors: {
